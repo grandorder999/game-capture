7463032c544f081ad07ccc95469c6619dbbd1e09
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-03-22 12:36:03 +0900

    チェッカーページ作成(仮)

---
+++ src/components/CheckPageButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <button
+      value="未取得"
+      type="button"
+      v-if="!flag"
+      v-on:click="changeButtonText"
+    >
+      未取得
+    </button>
+    <button
+      value="取得済"
+      type="button"
+      v-if="flag"
+      v-on:click="changeButtonText"
+    >
+      取得済
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class XXXComponent extends Vue {
+  @Prop()
+  private servantId!: number;
+
+  private flag = false;
+
+  @Emit()
+  changeButtonText(flag: boolean): void {
+    console.log(flag);
+  }
+  preClick(): void {
+    if (this.flag) {
+      this.flag = true;
+    } else {
+      this.flag = false;
+    }
+    // this.changeButtonText(this.flag);
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/CheckPage.vue
+++ src/views/CheckPage.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="container">
-    <div v-for="servant of currentServants" :key="servant.id">
-      <div>{{ servant.name }}</div>
-      <img :src="servant.face" />
+    <div class="servants">
+      <!-- <h2>🌟所持サーヴァントチェッカー🌟</h2> -->
+      <div v-for="servant of currentServants" :key="servant.id">
+        <img :src="servant.face" /><br />
+        <CheckPageButton
+          servant-id="servant.id"
+          v-on:change-button-text="onChange"
+        ></CheckPageButton>
+      </div>
+      <table border="1" width="250">
+        <tr>
+          <td>現在の所持数</td>
+          <td>
+            <span class="red">{{ possession }}</span
+            >/329
+          </td>
+        </tr>
+        <tr>
+          <td>現在コンプリート率</td>
+          <td>
+            <span class="blue">{{ complete }}</span
+            >%
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -10,15 +32,60 @@
 <script lang="ts">
 import { Servant } from "@/types/Servant";
 import { Component, Vue } from "vue-property-decorator";
-@Component
+import CheckPageButton from "@/components/CheckPageButton.vue";
+@Component({
+  components: {
+    CheckPageButton,
+  },
+})
 export default class XXXComponent extends Vue {
   private currentServants: Array<Servant> = [];
+  private possession = 0;
+  private complete = 0;
 
   async created(): Promise<void> {
     await this.$store.dispatch("getServants");
     this.currentServants = this.$store.getters.getServants;
   }
+
+  onChange(flag: boolean): void {
+    if (flag) {
+      this.possession++;
+    } else {
+      this.possession--;
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.servants {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 50%;
+}
+img {
+  width: 50px;
+  margin-right: 5px;
+  margin-left: 3px;
+}
+button {
+  margin-bottom: 5px;
+  margin-right: 5px;
+}
+table {
+  background-color: white;
+}
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+</style>
